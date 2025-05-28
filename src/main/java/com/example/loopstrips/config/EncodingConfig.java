f7ae6abd708ef96617cbca79bc4e694af00731e8
garsantsyan1 <garsantsyan1@gmail.com>
2025-05-28 03:19:09 +0300

    EncodingConfig добавил этот класс

---
+++ src/main/java/com/example/loopstrips/config/EncodingConfig.java
@@ -0,0 +1,22 @@
+package com.example.loopstrips.config;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class EncodingConfig {
+
+    @Bean
+    public FilterRegistrationBean<CharacterEncodingFilter> characterEncodingFilter() {
+        FilterRegistrationBean<CharacterEncodingFilter> registration = new FilterRegistrationBean<>();
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+}
