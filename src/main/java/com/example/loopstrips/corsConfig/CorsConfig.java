071bca194aa330c47cd0506aeeb76aaf3c6e5a63
garsantsyan1 <garsantsyan1@gmail.com>
2025-05-31 02:20:26 +0300

    добавил CorsConfig

---
+++ src/main/java/com/example/loopstrips/corsConfig/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.example.loopstrips.corsConfig;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/qr/**") // разрешаем CORS только для /qr/...
+                .allowedOrigins("https://loopstripes.ru",  "https://loopstripes.tilda.ws")  // домены, которые могут делать запросы
+                .allowedMethods("GET"); // какие HTTP методы разрешены
+    }
+}
